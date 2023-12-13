aeee496d5eef7a7fe906bf8167cfa6c5fd0dfdca
Stéphane Klein <contact@stephane-klein.info>
2023-12-13 16:24:43 +0100

    WIP

--- zig/playground.zig
+++ zig/playground.zig
@@ -1,24 +1,19 @@
 const std = @import("std");
 const print = @import("std").debug.print;
 
-pub fn wayland_display_connect() u16 {
+pub fn wayland_display_connect() std.net.Stream {
     var allocator = std.heap.page_allocator;
 
     const xdg_runtime_dir = std.os.getenv("XDG_RUNTIME_DIR");
-    print("Hello 1 {?s}\n", .{xdg_runtime_dir});
     const wayland_display = std.os.getenv("WAYLAND_DISPLAY");
-    print("Hello 1 {?s}\n", .{wayland_display});
 
-    const concatenatedString = std.fmt.allocPrint(allocator, "{?s}{?s}", .{xdg_runtime_dir, wayland_display}) catch unreachable;
+    const concatenatedString = std.fmt.allocPrint(allocator, "{?s}/{?s}", .{xdg_runtime_dir, wayland_display}) catch unreachable;
     print("{s}\n", .{concatenatedString});
-    const stream = std.net.connectUnixSocket("/run/user/1000/wayland-0") catch unreachable;
-    defer stream.close();
-    return 0;
+    const stream = std.net.connectUnixSocket(concatenatedString) catch unreachable;
+    return stream;
 }
 
 pub fn main() !void {
-    const stdout = std.io.getStdOut().writer();
-
-    const result = wayland_display_connect();
-    try stdout.print("Result {}\n", .{result});
+    const stream = wayland_display_connect();
+    defer stream.close();
 }
