4aafdc138c14449510c6bd41f017b76677d1e057
Stéphane Klein <contact@stephane-klein.info>
2023-12-13 16:26:55 +0100

    WIP

--- zig/playground.zig
+++ zig/playground.zig
@@ -4,10 +4,15 @@ const print = @import("std").debug.print;
 pub fn wayland_display_connect() std.net.Stream {
     var allocator = std.heap.page_allocator;
 
-    const xdg_runtime_dir = std.os.getenv("XDG_RUNTIME_DIR");
-    const wayland_display = std.os.getenv("WAYLAND_DISPLAY");
+    const concatenatedString = std.fmt.allocPrint(
+        allocator,
+        "{?s}/{?s}",
+        .{
+            std.os.getenv("XDG_RUNTIME_DIR"),
+            std.os.getenv("WAYLAND_DISPLAY")
+        }
+    ) catch unreachable;
 
-    const concatenatedString = std.fmt.allocPrint(allocator, "{?s}/{?s}", .{xdg_runtime_dir, wayland_display}) catch unreachable;
     print("{s}\n", .{concatenatedString});
     const stream = std.net.connectUnixSocket(concatenatedString) catch unreachable;
     return stream;
